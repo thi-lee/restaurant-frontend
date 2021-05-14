d43d7d9fb0e454e3fa70ee5fd9b2164c1b1fa927
Thi Le <thilehy7865@gmail.com>
2021-05-14 11:54:22 +0700

    log-in component

---
+++ src/app/log-in/log-in.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service'
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css']
+})
+export class LogInComponent implements OnInit {
+
+  constructor(
+    private as: AuthService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  async login(event: any, username: string, password: any) {
+    event.preventDefault();
+    let user = {
+      username: username,
+      password: password
+    }
+    await this.as.login(user);
+    await console.log('log in success')
+  }
+
+}
